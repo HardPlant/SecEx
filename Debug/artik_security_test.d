9e5cba3f3cbcae2cea1bee1327475c4891d5b03b
HardPlant <abc7988se@naver.com>
2018-05-30 20:13:48 +0900

    init

---
+++ Debug/artik_security_test.d
@@ -0,0 +1,25 @@
+artik_security_test.o: ../artik_security_test.c \
+ C:/ARTIK/SDK/A5_7_10/v1.6/sysroot/usr/include/artik/base/artik_module.h \
+ C:/ARTIK/SDK/A5_7_10/v1.6/sysroot/usr/include/artik/base/artik_types.h \
+ C:/ARTIK/SDK/A5_7_10/v1.6/sysroot/usr/include/artik/base/artik_list.h \
+ C:/ARTIK/SDK/A5_7_10/v1.6/sysroot/usr/include/artik/base/artik_error.h \
+ C:/ARTIK/SDK/A5_7_10/v1.6/sysroot/usr/include/artik/connectivity/artik_security.h \
+ C:/ARTIK/SDK/A5_7_10/v1.6/sysroot/usr/include/artik/base/artik_time.h \
+ C:/ARTIK/SDK/A5_7_10/v1.6/sysroot/usr/include/artik/base/artik_error.h \
+ C:/ARTIK/SDK/A5_7_10/v1.6/sysroot/usr/include/artik/base/artik_types.h
+
+C:/ARTIK/SDK/A5_7_10/v1.6/sysroot/usr/include/artik/base/artik_module.h:
+
+C:/ARTIK/SDK/A5_7_10/v1.6/sysroot/usr/include/artik/base/artik_types.h:
+
+C:/ARTIK/SDK/A5_7_10/v1.6/sysroot/usr/include/artik/base/artik_list.h:
+
+C:/ARTIK/SDK/A5_7_10/v1.6/sysroot/usr/include/artik/base/artik_error.h:
+
+C:/ARTIK/SDK/A5_7_10/v1.6/sysroot/usr/include/artik/connectivity/artik_security.h:
+
+C:/ARTIK/SDK/A5_7_10/v1.6/sysroot/usr/include/artik/base/artik_time.h:
+
+C:/ARTIK/SDK/A5_7_10/v1.6/sysroot/usr/include/artik/base/artik_error.h:
+
+C:/ARTIK/SDK/A5_7_10/v1.6/sysroot/usr/include/artik/base/artik_types.h:
